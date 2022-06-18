059636522064f066fa5be097ecba7d023aac5fcd
AlierQ <1029369938@qq.com>
2022-06-18 16:32:39 +0800

    webpack生成HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
     mode:"development",
@@ -7,4 +8,15 @@ module.exports = {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "我是默认生成的index.html"
+        }), //默认的
+        new HtmlWebpackPlugin({ // 使用自己编写的
+            // title: 'My App',  // 设置html的title
+            title: "这是我自己的html",
+            filename: 'test.html', // 指定html文件所在位置
+            template: 'assets/test.html'
+        })
+    ],
 };
